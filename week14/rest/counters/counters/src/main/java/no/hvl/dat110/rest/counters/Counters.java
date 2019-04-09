3a1bf72742116fb6692c847002aebee9fc461ab5
Lars Kristensen <lmkr@hib.no>
2019-04-09 13:27:57 +0200

    Added implementation of REST counter service

---
+++ week14/rest/counters/counters/src/main/java/no/hvl/dat110/rest/counters/Counters.java
@@ -0,0 +1,28 @@
+package no.hvl.dat110.rest.counters;
+
+public class Counters {
+	
+	private int red,green;
+
+	public Counters () {
+		this.red = 0;
+		this.green = 0;
+	}
+
+	public int getRed() {
+		return red;
+	}
+
+	public void setRed(int red) {
+		this.red = red;
+	}
+
+	public int getGreen() {
+		return green;
+	}
+
+	public void setGreen(int green) {
+		this.green = green;
+	}
+	
+}
